7bb40409ba4a29943b64f3c43f05e92e7303c325
Bryce Perez <bryceperez31@gmail.com>
2019-02-22 12:23:57 -0700

    all but lodash

--- app4/src/App.js
+++ app4/src/App.js
@@ -8,22 +8,34 @@ class App extends Component {
     this.state = {
       cars: []
     }
+    this.getCars=this.getCars.bind(this);
   }
 
   getCars() {
-    axios.get('https://joes-autos.herokuapp.com/api/vehicles')
-    .then(res => {
-      this.setState({
-        cars: res
-      })
+    axios.get('https://joes-autos.herokuapp.com/api/vehicles').then(res => {
+        this.setState({
+        cars: res.data
+     })
     })
   }
 
   render() {
+    const carz = this.state.cars.map(car => {
+      return (
+        <div key={car.id}>
+          <p>make: {car.make}</p>
+          <p>model: {car.model}</p>
+          <p>year: {car.year}</p>
+          <p>color: {car.color}</p>
+          <p>price: {car.price}</p>
+          <br />
+        </div>
+      )
+    })
     return (
       <div className="App">
         <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+          {carz}
       </div>
     );
   }
